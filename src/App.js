1dca2e6cd823480c4454bc1af48be513875f7403
louyang <louyang367@gmail.com>
2018-04-02 16:09:32 -0700

    new component RoomList and other code to display list of rooms

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+import RoomList from './components/RoomList';
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBZlFvW0Gq4iAR5Uux7Jn2NKEAkAKyyl3s",
+    authDomain: "bloc-chat-2db5d.firebaseapp.com",
+    databaseURL: "https://bloc-chat-2db5d.firebaseio.com",
+    projectId: "bloc-chat-2db5d",
+    storageBucket: "bloc-chat-2db5d.appspot.com",
+    messagingSenderId: "402057190456"
+  };
+  firebase.initializeApp(config);
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <nav>
+          <h1 className="App-title">Bloc Chat</h1>
+          <RoomList className='RoomList' firebase={firebase} />
+        </nav>
       </div>
-    );
+    )
   }
 }
 
